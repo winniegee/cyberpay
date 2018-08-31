c6b7a05de06385d2ff7c90cd9cfd0bca49e2a974
Winnie <Winnie@DESKTOP-E0E185B>
2018-08-31 12:25:00 +0100

    first

---
+++ CyberPay.Cmd/Payload/Quickteller/QuickTellerBanksViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberPay.Cmd.Payload.Quickteller
+{
+    public class QuickTellerBanksViewModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("cbnCode")]
+        public string CBNCode { get; set; }
+        [JsonProperty("bankName")]
+        public string BankName { get; set; }
+        [JsonProperty("bankCode")]
+        public string BankCode { get; set; }
+    }
+}
